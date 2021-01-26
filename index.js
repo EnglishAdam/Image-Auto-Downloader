365dbe4ab144f52722c53e91dc338e77bf01e993
Adam English <englishadam@hotmail.co.uk>
2021-01-26 20:11:32 +0000

    Added comments and refinements

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
+// Required modules
 const https = require('https')
 const fs = require('fs')
 
+/**
+ * Saves a file at a url to a set path, must ensure folder exists before hand
+ * @param {*} path Path to save file to
+ * @param {*} url Url to fetch image from
+ */
 function saveFile(path, url) {
+  // Create and reference stream
   const file = fs.createWriteStream(path);
+
+  // Fetch the resource at the end of the url
   https.get(url, ({ headers }) => {
-    https.get(headers.location, (res) => {
+    if (headers.location) {
+      // If the url returns a resource location (Such as with picsum or unsplashed)
+      // Thenr request the resource at their location
+      https.get(headers.location, (res) => {
+        // Store into the file
+        res.pipe(file);
+      }).on('error', (e) => console.error(e))
+    } else {
+      // Else, if the url is directly to the resouce
+      // Store into the file
       res.pipe(file);
-    }).on('error', (e) => console.error(e))
-  })
+    }
+  }).on('error', (e) => console.error(e))
 }
 
-const number = 10
-const sizes = [
-  [512, 512], // Small image
-  [1280, 720]
-]
+// Number of images
+const count = 10 
 
-for (let index = 0; index < number; index++) {
+// For each count fetches small and large image from picsum and stores it
+for (let index = 0; index < count; index++) {
   saveFile(`512x512/${index}.jpg`, 'https://picsum.photos/512/512')
   saveFile(`1280x720/${index}.jpg`, 'https://picsum.photos/1280/720')
 }
